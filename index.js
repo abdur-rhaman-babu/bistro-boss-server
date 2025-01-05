7e450a76dc7d1be4371cf21b658cd426a5b7b265
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-05 22:20:46 +0600

    get data with specific email

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,30 +27,35 @@ async function run() {
     const cartCollections = client.db("bistroDB").collection("carts");
 
     // create get api for menu
-    app.get('/menu', async (req, res)=>{
-        const result = await menuCollections.find().toArray()
-        res.send(result)
-    })
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    });
 
     // create get api for reviews
-    app.get('/reviews', async (req,res)=>{
-        const result = await reviewCollections.find().toArray()
-        res.send(result)
-    })
-    
-    // create post api for cart 
-    app.post('/carts', async (req, res)=>{
-      const cartItem = req.body;
-      const result = await cartCollections.insertOne(cartItem)
-      res.send(result)
-    })
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollections.find().toArray();
+      res.send(result);
+    });
 
+    // create post api for cart
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollections.insertOne(cartItem);
+      res.send(result);
+    });
+
+    // create get api for carts
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartCollections.find(query).toArray();
+      res.send(result);
+    });
   } finally {
-   
   }
 }
 run().catch(console.dir);
-
 
 app.get("/", (req, res) => {
   res.send("Bistro server is sitting....");
