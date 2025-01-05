476af03abb7e06667144b0bc203af30f6ee428e2
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-05 20:55:34 +0600

    post api for add to cart

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require("express");
 const cors = require("cors");
 
@@ -7,6 +8,50 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6avkk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const menuCollections = client.db("bistroDB").collection("menu");
+    const reviewCollections = client.db("bistroDB").collection("reviews");
+    const cartCollections = client.db("bistroDB").collection("carts");
+
+    // create get api for menu
+    app.get('/menu', async (req, res)=>{
+        const result = await menuCollections.find().toArray()
+        res.send(result)
+    })
+
+    // create get api for reviews
+    app.get('/reviews', async (req,res)=>{
+        const result = await reviewCollections.find().toArray()
+        res.send(result)
+    })
+    
+    // create post api for cart 
+    app.post('/carts', async (req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollections.insertOne(cartItem)
+      res.send(result)
+    })
+
+  } finally {
+   
+  }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
   res.send("Bistro server is sitting....");
