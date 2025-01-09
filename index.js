650b1a0d2a03fcc953e15d742e990bf4e8b2917c
abdur rhaman babu <abdurrahmanbab74@gamil.com>
2025-01-09 12:42:32 +0600

    create delete api for menu

--- index.js
+++ index.js
@@ -78,6 +78,14 @@ async function run() {
         res.send(result)
     })
 
+    // create delele api for menu
+    app.delete('/menu/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result =  await menuCollections.deleteOne(query)
+      res.send(result)
+    })
+
     // create get api for reviews
     app.get("/reviews", async (req, res) => {
       const result = await reviewCollections.find().toArray();
